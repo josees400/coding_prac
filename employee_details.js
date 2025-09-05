f5936784d7de6b794880b42856a00781325792ad
joes7355 <joes7355@colorado.edu>
2025-09-04 18:03:59 -0600

    fixed up to last step

--- employee_details.js
+++ employee_details.js
@@ -1,14 +1,14 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000},
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000},
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000},
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript'},
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python'},
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java'},
     //...More employees records can be added here 
 ]
 
 //Function to display all employees, map command maps all elements of each individual employee
 function displayEmployees() {
     const totalEmployees = employees
-        .map(employee => <p>${employees.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>)
+        .map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`)
         .join('');
     document.getElementById('employeeDetails').innerHTML = totalEmployees;
     }
@@ -23,16 +23,16 @@ function calculateTotalSalaries() {
 function displayHREmployees() {
     const hrEmployees = employees.filter(employee => employee.department =='HR');
     const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
-    document.getElementById('employeesDetails').innerHTML = hrEmployees.display;
+    document.getElementById('employeeDetails').innerHTML = hrEmployeesDisplay;
 }
 
 // In this fucnction we are using the find command which looks for a value that matches the given value
 function findEmployeeById(employeeId){
     const foundEmployee = employees.find(employee => employee.id === employeeId);
     if (foundEmployee){
-        document.getElementById('employeeDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`.join('');
+        document.getElementById('employeeDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
     }
     else{
-        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID'
+        document.getElementById('employeeDetails').innerHTML = 'no employee has been found with this ID'
     }
 }
